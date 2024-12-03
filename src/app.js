01c62e9a0c6aa9b6516bfa005c21ae725538e51e
Maksymilian Piechota <maksymilian.piechota@gmail.com>
2024-12-03 09:16:12 +0100

    Serverless Express server repo draft

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+// Definicje endpointów
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World! This is running on Express!' })
+})
+
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: 'Test endpoint working!' })
+})
+
+module.exports = app
